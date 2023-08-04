9136535b082103b57619555cf0b77e5a68ffbfa8
dope0515 <61546301+dope0515@users.noreply.github.com>
2023-08-04 16:23:02 +0900

    230804

--- react/blog/src/App.js
+++ react/blog/src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
+import {useState} from 'react';
 import './App.css';
 
 function App() {
+  
+  let [title,titleValue] = useState(['글제목1','글제목2','글제목3','글제목4']);
+  let [thumbUp, setthumbUp] = useState([0,0,0,0]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="black-nav">
+        <div>React blog</div>
+      </div>
+      <button onClick={()=>{
+        let copy = [...title];
+            copy.sort(function(a,b){return a - b;});
+            titleValue(copy);
+          }
+        }>수정버튼</button>
+      {
+        title.map(function(_,i){
+          return(
+            <div className="list">
+              <h4>
+                {title[i]}
+                <span onClick={ ()=>{ 
+                  let copy = [...thumbUp];
+                  copy[i] = copy[i] + 1;
+                  setthumbUp(copy)
+                 } }>👍</span>{thumbUp[i]}  
+              </h4>
+              <p>날짜</p>
+              <p>상세내용</p>
+            </div>
+          )
+        })
+      }
+      <Modal title={title} bg="#f1f1f1" txtcolor="#222"></Modal>
     </div>
   );
 }
 
+function Modal(props){
+  return(
+    <div className="modal" style={{backgroundColor: props.color,color: props.txtcolor}}>
+      <h4>{props.title[0]}</h4>
+      <p>날짜</p>
+      <p>상세내용</p>
+    </div>
+  );
+};
 export default App;
